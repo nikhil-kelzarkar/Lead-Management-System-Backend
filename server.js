2171cd5b0ee1a158872c6928def913f6e8f4dec7
Nikhil Dnyneshwar Kelzarkar <159932584+nikhil-kelzarkar@users.noreply.github.com>
2025-06-28 01:26:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'));
+
+app.use('/api/leads', require('./routes/leads'));
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
